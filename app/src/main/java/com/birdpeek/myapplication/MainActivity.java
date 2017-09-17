ce48e604a58cff1639740978cee7b14191c745c0
han <afaideen@gmail.com>
2017-09-17 23:37:02 +0800

    'message'

---
+++ app/src/main/java/com/birdpeek/myapplication/MainActivity.java
@@ -0,0 +1,74 @@
+package com.birdpeek.myapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.birdpeek.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+//ref https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+//or https://cloud.google.com/tools/android-studio/app_engine/run_test_deploy
+
+import java.io.IOException;
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+//                    .setRootUrl("http://192.168.0.180:8080/_ah/api/")//for local test. done ok!
+                    .setRootUrl("https://1-dot-playchat-53073.appspot.com/_ah/api/")//for deployment
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+}
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+    }
+
+
+}
+
+
